96dde25f06e7c7263f4aa4f8d66dd363ee54e573
Kristiyan <komapeb@gmail.com>
2019-06-02 12:36:10 +0300

    initial commit

---
+++ bin/main.dart
@@ -0,0 +1,84 @@
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+// extending Equatable, so we can deeply compare instances
+@immutable
+class DataItem extends Equatable {
+  final String name;
+  final int age;
+  final String image;
+
+  DataItem(this.name, this.age, this.image) : super([name, age, image]);
+}
+
+@immutable
+class AppState extends Equatable {
+  final List<DataItem> items;
+
+  // these are just for example, not actually used
+  final int someOtherProp;
+  final String otherState;
+
+  AppState(
+    this.items, [
+    this.someOtherProp,
+    this.otherState,
+  ]) : super([
+          items,
+          someOtherProp,
+          otherState,
+        ]);
+}
+
+main(List<String> arguments) {
+  // example data states that might be fetched from an external source
+  AppState currentState = AppState(<DataItem>[
+    DataItem('john1', 16, 'img1.png'),
+    DataItem('john2', 17, 'img2.png'),
+    DataItem('john3', 18, 'img3.png'),
+    DataItem('john4', 19, 'img4.png'),
+    DataItem('john5', 20, 'img5.png'),
+    DataItem('john6', 21, 'img6.png'),
+    DataItem('john7', 22, 'img7.png'),
+    DataItem('john8', 23, 'img8.png'),
+    DataItem('john9', 24, 'img9.png'),
+  ]);
+
+  // removing 1, 4, 7, 9, adding 10, 13, 15, 19
+  AppState nextState = AppState(<DataItem>[
+    DataItem('john2', 17, 'img2.png'),
+    DataItem('john3', 18, 'img3.png'),
+    DataItem('john5', 20, 'img5.png'),
+    DataItem('john6', 21, 'img6.png'),
+    DataItem('john8', 23, 'img8.png'),
+    DataItem('john10', 25, 'img10.png'),
+    DataItem('john13', 28, 'img13.png'),
+    DataItem('john15', 30, 'img15.png'),
+    DataItem('john19', 34, 'img19.png'),
+  ]);
+
+  List<DataItem> removedItems = <DataItem>[];
+  List<DataItem> addedItems = <DataItem>[];
+
+  List<DataItem> u1 = <DataItem>[];
+  List<DataItem> u2 = <DataItem>[];
+
+  for (final item in nextState.items) {
+    if (!currentState.items.contains(item)) {
+      addedItems.add(item);
+    } else {
+      u1.add(item);
+    }
+  }
+
+  for (final item in currentState.items) {
+    if (!nextState.items.contains(item)) {
+      removedItems.add(item);
+    } else {
+      u2.add(item);
+    }
+  }
+
+  print("ADDED: $addedItems");
+  print("REMOVED: $removedItems");
+}
